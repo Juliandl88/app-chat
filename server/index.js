7a5fa6b27c185012c656a195501a077e37a5a2dc
Julian <julian_delisio@hotmail.com>
2022-11-15 23:22:36 -0300

    socket

---
+++ server/index.js
@@ -0,0 +1,44 @@
+// server/index.js
+
+const express = require("express");
+const app = express();
+http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io"); // Add this
+
+app.use(cors()); // Add cors middleware
+
+const server = http.createServer(app); // Add this
+
+// Add this
+// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+// Add this
+// Listen for when the client connects via socket.io-client
+const CHAT_BOT = "ChatBot"; // Add this
+// Listen for when the client connects via socket.io-client
+io.on("connection", (socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  // We can write our socket event listeners in here...
+  socket.on("join_room", (data) => {
+    const { username, room } = data; // Data sent from client when join_room event emitted
+    socket.join(room); // Join the user to a socket room
+
+    let __createdtime__ = Date.now(); // Current timestamp
+   // Send welcome msg to user that just joined chat only
+   socket.emit('receive_message', {
+    message: `Welcome ${username}`,
+    username: CHAT_BOT,
+    __createdtime__,
+    });
+  });
+});
+
+server.listen(4000, () => "Server is running on port 3000");
